0383019debddce1aa3a9b351c6800be4105f7ade
Libo Su <l.su@ixp-duesseldorf.de>
2021-11-28 19:39:58 +0100

    add package skeleton

---
+++ setup.py
@@ -0,0 +1,14 @@
+# ! Do not manually invoke this setup.py. Use catkin instead
+
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=["audio_recorder"],
+    package_dir={"audio_recorder": "src/audio_recorder"}
+)
+
+setup(**setup_args)
