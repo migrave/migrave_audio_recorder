28fec3f57417e47419f31016bf359c21bd438e54
Libo Su <l.su@ixp-duesseldorf.de>
2022-03-07 16:58:02 +0100

    update package structure

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=["audio_recorder"],
-    package_dir={"audio_recorder": "src/audio_recorder"}
+    package_dir={"audio_recorder": "ros/src/audio_recorder"}
 )
 
 setup(**setup_args)
