e09d2a5717fb96a817a6da158d8db680bda8eddb
Libo Su <l.su@ixp-duesseldorf.de>
2022-03-08 18:33:34 +0100

    associated saved file with participant id

--- ros/src/audio_recorder/audio_recorder.py
+++ ros/src/audio_recorder/audio_recorder.py
@@ -1,7 +1,7 @@
 import wave
 from pathlib import Path
 import datetime
-
+import rospy
 
 class AudioRecorder:
     def __init__(
@@ -25,14 +25,30 @@ class AudioRecorder:
 
         if self._is_recording:
             raise RuntimeError("Audio is already being recorded")
-        if not Path(self._out_directory).is_dir():
-            Path(self._out_directory).mkdir()
+
+        # parameter set by migrave_games
+        parameter_name = "/migrave/game_performance/participant_id"
+        if rospy.has_param(parameter_name):
+            participant_id = rospy.get_param(parameter_name)
+        if participant_id == "":
+            rospy.loginfo("Participant ID not set!")
+            participant_id = "ID_Unknown"
+
+        self._out_directory_id = Path(self._out_directory) / participant_id
+        if not Path(self._out_directory_id).is_dir():
+            Path(self._out_directory_id).mkdir(parents=True, exist_ok=True)
+
         if out_file_name is None:
             ext = self._audio_type
-            file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            now = datetime.datetime.now()
+            # time in YYYY-MM-DD_HH_MM-SS format
+            time = now.strftime("%Y-%m-%d_%H-%M-%S")
+            # unixtimestamp 16 digits
+            stamp = int(datetime.datetime.timestamp(now) * 1000000)
+            file_name = f"{time}_{stamp}"
             out_file_name = f"{file_name}.{ext}"
 
-        out_file_path = Path(self._out_directory) / out_file_name
+        out_file_path = Path(self._out_directory_id) / out_file_name
         out_file_path = str(out_file_path)
         self._wf = wave.open(out_file_path, "wb")
         self._wf.setnchannels(self._audio_channels)
